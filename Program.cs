f2fbe1a90ba008b4d3febf473c4f800d472bc9a2
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2022-11-21 11:14:33 -0400

    minor changes

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace StringFormatter
     internal class Program
     {
 
-        static string story = "This is my story. It is a very long story and will be treated as if pulled in from an outside file and needs to be treated in some way, so that my program can then properly display it on a page in order to create an interactive fiction experience.";
+        //static string story = "This is my story. It is a very long story and will be treated as if pulled in from an outside file and needs to be treated in some way, so that my program can then properly display it on a page in order to create an interactive fiction experience.";
 
+        static string[] story = { "This is my story. It is a very long story and will be treated as if pulled in from an outside file and needs to be treated in some way, so that my program can then properly display it on a page in order to create an interactive fiction experience."; " };
         static void Main(string[] args)
         {
+
             Console.WriteLine(story);
             Console.ReadKey(true);
 
